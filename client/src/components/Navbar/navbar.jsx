4effc7a37fe0bf0a44f27f3d8be8da7c183381a3
Tarun <tarunlohchab43210@gmail.com>
2025-09-04 22:16:43 +0530

    Fix duplicate React import in Navbar

--- client/src/components/Navbar/navbar.jsx
+++ client/src/components/Navbar/navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useCallback } from 'react';
-import React, { useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useSelector, useDispatch } from "react-redux"
 import bars from '../../assets/bars-solid.svg'
